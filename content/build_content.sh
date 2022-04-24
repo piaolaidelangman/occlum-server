e2ca7ae7ee2e6e53aeabde9c98f28b814cd77585
piaolaidelangman <707056129@qq.com>
2022-04-24 19:49:02 +0800

    Update

--- content/build_content.sh
+++ content/build_content.sh
@@ -11,9 +11,9 @@ function build_ratls()
 {
     rm -rf ${DEP_LIBS_DIR} && mkdir ${DEP_LIBS_DIR}
     pushd ${RATLS_DIR}
-    ./download_and_prepare.sh
-    ./build_and_install.sh musl
-    ./build_occlum_instance.sh musl
+    bash ./download_and_prepare.sh
+    bash ./build_and_install.sh musl
+    bash ./build_occlum_instance.sh musl
 
     cp ./grpc-src/examples/cpp/ratls/build/libgrpc_ratls_client.so ${DEP_LIBS_DIR}/
     cp ./grpc-src/examples/cpp/ratls/build/libhw_grpc_proto.so ${DEP_LIBS_DIR}/
